87e754810059fff807734ef7f1bb9d75bb9c80b6
Eddy Ernesto del Valle Pino <xigmatron@gmail.com>
2018-02-02 14:58:31 +0100

    Add undistract me to fish.

--- config.fish
+++ config.fish
@@ -33,9 +33,69 @@ set __fish_git_prompt_char_upstream_ahead '+'
 set __fish_git_prompt_char_upstream_behind '-'
 
 
+set __fish_preexec_command ""
+set __fish_preexec_timestamp (date +'%s')
+set __fish_preexec_status 0
+
+
+function preexec --on-event fish_preexec
+  set __fish_preexec_command $argv
+  set __fish_preexec_timestamp (date +'%s')
+end
+
+
+function postexec --on-event fish_postexec
+  set __fish_preexec_status $status
+end
+
+function sec_to_human
+    set -l H ""
+    set -l M ""
+    set -l S ""
+
+    set -l h (math $argv[1] / 3600)
+    if test $h -gt 0
+        set H "$h hour"
+        if test $h -gt 1
+           set H (string join "" "$H" "s")
+        end
+    end
+
+    set -l m (math $argv[1] / 60 \% 60)
+    if test $m -gt 0
+        set M "$m min"
+        if test $m -gt 1
+           set M (string join "" "$M" "s")
+        end
+    end
+
+    set -l s (math $argv[1] \% 60)
+    if test $s -gt 0
+        set S "$s sec"
+        if test $s -gt 1
+           set S (string join "" "$S" "s")
+        end
+    end
+
+    echo (echo $H $M $S | tr -s " ")
+end
+
 function fish_prompt --description 'Write out the prompt'
     set -l color_cwd
     set -l suffix
+    set -l sec_num 5
+
+    if test $CMD_DURATION -ge (math "1000 * $sec_num")
+       set -l command_time (math (date +'%s') - $__fish_preexec_timestamp)
+       set command_time (sec_to_human $command_time)
+
+       set -l icon "terminal"
+       if test $__fish_preexec_status -ne 0
+         set icon "dialog-error"
+       end
+       mpv /usr/share/sounds/freedesktop/stereo/complete.oga --really-quiet & \
+         notify-send -i $icon "Long command completed" "$__fish_preexec_command\nTook $command_time"
+    end
 
     if set -q VIRTUAL_ENV
        echo -n -s "(" (basename "$VIRTUAL_ENV") ") "
